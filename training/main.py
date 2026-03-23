20e2df93e918b25430239b475c773a4833f5ba88
Joshua <joshuadayal72@gmail.com>
2026-03-23 07:54:56 -0400

    Finished Training file

--- training/main.py
+++ training/main.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from config import WATCHLIST, DATA_CONFIG
 from data.fetcher import FetchAll
 from data.features import BuildDataset
